4f35795c1bc15d616ee9cc459f8931ac122cd10d
Nattida Wong <nattidac.wong@gmail.com>
2020-11-25 21:50:24 -0500

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,15 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new(
+    title: "The Matrix", 
+    release_date: "1999", 
+    director: "The Wachowski Sisters", 
+    lead: "Keanu Reeves", 
+    in_theaters: false
+    )
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
